4c3212795ff32da815e30308d96aefd91c117f80
avyfain <avyfain@gmail.com>
2014-09-07 18:36:32 -0500

    added twitter logic and buttons

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,19 @@ class TweetsController < ApplicationController
   def index
     username = 'opternative'
     options = {:count => 100, :include_rts => true, :exclude_replies => true}
-    @tweets = $client.user_timeline(username, options)[0..25]
+
+    if params[:kind] == 'links'
+        @tweets = $client.user_timeline(username, options)
+        @tweets = @tweets.select {|t| t.urls?}[(0..25)]
+        
+    elsif params[:kind] == 'others'
+        @tweets = $client.search("to:" + username, options).take(25)
+
+    elsif params[:kind] == 'recent'
+        @tweets = $client.user_timeline(username, options)
+        @tweets = @tweets.select { |t| (t.created_at >= (Date.today - 7.days))}
+    else
+        @tweets = $client.user_timeline(username, options)[(0..25)]
+    end
   end
 end
