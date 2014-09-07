cd547a5c7f5ca506437f243891024971805dd42c
avyfain <avyfain@gmail.com>
2014-09-06 21:27:37 -0500

    removing env vars from git

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 $client = Twitter::REST::Client.new do |config|
-  config.consumer_key = 'tq4RbQN7ts061VHhm1B7hsVPN'
-  config.consumer_secret = 'gBKsCxSGGME5ChZktKVSqxonfI7TaiDfPvr63c1baVhD971dBU'
-  config.oauth_token = '17587308-ZOC1hIIVElJEvjx0aDpTHe4RWfgxqnCLWhL57MHm8'
-  config.oauth_token_secret = '26feQDRShKY5N6m8n3H9u9zVYpv0Dr6j8THo3fejvEl8G'
+  config.consumer_key        = ENV['CONSUMER_KEY']
+  config.consumer_secret     = ENV['CONSUMER_SECRET']
+  config.oauth_token         = ENV['ACCESS_TOKEN']
+  config.oauth_token_secret  = ENV['ACCESS_SECRET']
 end
